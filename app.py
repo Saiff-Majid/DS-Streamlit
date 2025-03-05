1462fec3bfd3feffa925123cb359eece67dc82d0
Rachel <r.hill.tsarpelas@gmail.com>
2025-03-05 10:38:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -127,14 +127,19 @@ elif page == "Data Preprocessing & Cleaning":
     # Select only the filtered columns from the original DataFrame
     df_filtered = df_merged[filtered_columns]
 
-    # Apply CSS to increase row height for better fitting
+   # Apply CSS to wrap column names and adjust row height
     st.markdown("""
-    <style>
-        .stDataFrame tbody tr th, .stDataFrame tbody tr td {
-            padding: 15px;  /* Increase padding for better height */
-            height: 60px;   /* Increase row height */
-        }
-    </style>
+        <style>
+            .stDataFrame thead th {
+            white-space: normal;  /* Allow column names to wrap */
+            word-wrap: break-word;  /* Break long words onto the next line */
+            max-width: 200px;  /* Optional: Set a max-width for the header */
+            }
+            .stDataFrame tbody tr th, .stDataFrame tbody tr td {
+                padding: 15px;  /* Increase padding for better height */
+                height: 60px;   /* Increase row height */
+            }
+        </style>
     """, unsafe_allow_html=True)
 
     # Display the filtered dataframe
